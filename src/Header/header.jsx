6927885b302d06e5642b19cb920afe6bc0b8fe19
fauzipradipta <dipta0988@gmail.com>
2025-01-08 00:58:56 +0700

    add header

---
+++ src/Header/header.jsx
@@ -0,0 +1,30 @@
+
+
+function header(){
+
+    return (
+        <div className="Header">
+
+            <nav>
+                    {/* <a href="https://vite.dev" target="_blank"> */}
+                <a>
+                    <p>Azhim Firdaus </p>
+                </a>
+
+                <a>
+                    <p> Portofolio</p>
+                </a>
+                <a>
+                    <p> Linkedin</p>
+                </a>
+                <a>
+                    <p> Contact</p>
+                </a>
+            </nav>
+            
+            
+        </div>
+    )
+}
+
+export default header;
