dffd005795ec23cee73836b23df5da8960d33264
Syawadhilah Pradipta <fpradipta@yahoo.com>
2025-01-11 14:00:14 +0700

    base navbar

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,29 +1,19 @@
-
+import React from 'react';
+import '../Header/Header.css'
 
 function header(){
 
     return (
-        <div className="Header">
-
-            <nav>
-                    {/* <a href="https://vite.dev" target="_blank"> */}
-                <a>
-                    <p>Azhim Firdaus </p>
-                </a>
-
-                <a>
-                    <p> Portofolio</p>
-                </a>
-                <a>
-                    <p> Linkedin</p>
-                </a>
-                <a>
-                    <p> Contact</p>
-                </a>
-            </nav>
-            
-            
-        </div>
+        <div className="header">
+        <h1 className="logo">Azhim Firdaus</h1>
+        <nav>
+          <ul>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#linkedin">LinkedIn</a>
+            <a href="#contact">Contact</a>
+          </ul>
+        </nav>
+      </div>
     )
 }
 
